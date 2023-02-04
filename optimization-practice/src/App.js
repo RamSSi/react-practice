5f96e2bdb1964dc7f5e3ec8d065a12deff52ed51
강아람 <001229aram@gmail.com>
2023-02-04 22:02:05 +0900

    2023-02-04 optimization 완료

--- optimization-practice/src/App.js
+++ optimization-practice/src/App.js
@@ -1,27 +1,57 @@
-import React, { useCallback, useState } from "react";
-import Button from "./components/UI/Button/Button";
+// import React, { useCallback, useState } from "react";
+// import Button from "./components/UI/Button/Button";
+// import "./App.css";
+// import DemoOutput from "./components/Demo/DemoOutput";
+
+// function App() {
+//   const [showParagraph, setShowParagraph] = useState(false);
+//   const [allowToggle, setAllowToggle] = useState(false);
+
+//   const toggleParagraphHandler = useCallback(() => {
+//     if (allowToggle) {
+//       setShowParagraph(prevShowParagraph => !prevShowParagraph);
+//     }
+//   }, [allowToggle]);
+
+//   const allowToggleHandler = () => {
+//     setAllowToggle(prevAllowToggle => !prevAllowToggle);
+//   };
+
+//   console.log("APP RUNNING");
+//   return (
+//     <div className="app">
+//       <h1>Hi there!</h1>
+//       <DemoOutput show={showParagraph} />
+//       <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
+//       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useCallback, useMemo } from "react";
+
 import "./App.css";
-import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }, [allowToggle]);
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
 
-  const allowToggleHandler = () => {
-    setAllowToggle(prevAllowToggle => !prevAllowToggle);
-  };
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
-  console.log("APP RUNNING");
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
-      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+      <DemoList
+        title={listTitle}
+        items={listItems}
+      />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
